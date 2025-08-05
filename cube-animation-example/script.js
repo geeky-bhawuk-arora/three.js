3828222f696ade612586292c2ffa6715bee052d0
Bhawuk Arora <bhawukarora042@gmail.com>
2025-08-05 07:51:20 +0530

    g2g

--- cube-animation-example/script.js
+++ cube-animation-example/script.js
@@ -1,3 +1,5 @@
+// import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -9,13 +11,16 @@ scene.add(cube);
 camera.position.z = 5;
 
 const canvas = document.querySelector("#draw");
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight); 
+
+// const clock = new THREE.Clock();
 
 function animate() {
     window.requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += clock.getElapsedTime();
+    cube.rotation.z += 0.01;
     renderer.render(scene, camera);
 }
 
